b445d8a6f75953be9ef2c3ace702848db481097d
andrew <andrew@andrew-VirtualBox>
2014-05-18 19:42:21 +0100

    Corrected folder structure

--- andrewform/lib/Drupal/andrewform/Form/AndrewformForm.php
+++ andrewform/lib/Drupal/andrewform/Form/AndrewformForm.php
@@ -1,8 +1,50 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+* @file
+* Contains \Drupal\andrewform\Form\AndrewformForm.
+*/
+namespace Drupal\andrewform\Form;
+use Drupal\Core\Form\FormBase;
+/**
+* Implements an example form.
+*/
+class AndrewformForm extends FormBase {
+  /**
+   * {@inheritdoc}.
+   */
+  public function getFormId() {
+    return 'andrewform_form';
+  }
+  /**
+   * {@inheritdoc}.
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $form['phone_number'] = array(
+      '#type' => 'tel',
+      '#title' => $this->t('Your phone number')
+    );
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    );
+    return $form;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, array &$form_state) {
+    if (strlen($form_state['values']['phone_number']) < 3) {
+      $this->setFormError('phone_number', $form_state, $this->t('The phone number is too short. Please enter a full phone number.'));
+    }
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    drupal_set_message($this->t('Your phone number is @number', array('@number' => $form_state['values']['phone_number'])));
+  }
+}
+?>
 
