50e987695a64c8fe0a0a19d27d2990477a8620f2
andrew <andrew@andrew-VirtualBox>
2014-05-18 19:27:15 +0100

    added folder structure

--- andrewform/lib/Drupal/andrewform/Controller/AndrewformController.php
+++ andrewform/lib/Drupal/andrewform/Controller/AndrewformController.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace Drupal\andrew\Controller;
+namespace Drupal\andrewform\Controller;
 
 use Drupal\Core\Controller\ControllerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class AndrewController implements ControllerInterface {
+class AndrewformController implements ControllerInterface {
    
     public static function create(ContainerInterface $container) {
         return new static($container->get('module_handler'));
     }
     
-    public function andrewPage() {
+    public function andrewformPage() {
         $build = array(
             '#type' => 'markup',
             '#markup' => t('Hello World!'),
